f2324485eeed35f5b64fa8402adcc593fc47cc4e
Антон Красноперов <an2niy85@mail.ru>
2021-11-17 23:02:42 +0400

    Добавьте файлы проекта.

---
+++ Questionnaire/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questionnaire
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            byte age;
+            string zodiac;
+            string work;
+
+            Console.WriteLine("Как вас зовут?");
+            name = Console.ReadLine();
+            Console.WriteLine("Сколько вам лет?");
+            age = Convert.ToByte(Console.ReadLine());
+            Console.WriteLine("Какой ваш знак зодиака?");
+            zodiac = Console.ReadLine();
+            Console.WriteLine("Где вы работаете?");
+            work = Console.ReadLine();
+
+            Console.WriteLine("Вас зовут " + name + ", вам " + age + " год, вы " + zodiac + " и работаете на " + work);
+            Console.ReadLine();
+        }
+    }
+}
